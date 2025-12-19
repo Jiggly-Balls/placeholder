477f0bb9313d79d6e7b86d6dd8991ed3ac9fd48b
Jiggly Balls <jigglyballs9000@gmail.com>
2025-12-19 20:28:53 +0530

    Renamed file from base to base_state

--- states/meta/base_state.py
+++ states/meta/base_state.py
@@ -7,11 +7,15 @@ from game_state import State
 if TYPE_CHECKING:
     from pykraken import Event
 
+    from states.meta.base_manager import BaseManager
+
 
 __all__ = ("BaseState",)
 
 
 class BaseState(State["BaseState"]):
+    manager: BaseManager  # pyright: ignore[reportIncompatibleVariableOverride]
+
     def process_update(self, dt: float) -> None:  # pyright: ignore[reportIncompatibleMethodOverride]
         ...
 
