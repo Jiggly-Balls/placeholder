a399cc34dc41a5bfb6bfff88654f993e4fa833b9
Jiggly Balls <jigglyballs9000@gmail.com>
2025-12-19 19:45:24 +0530

    Added process_event to BaseState

--- states/meta/base.py
+++ states/meta/base.py
@@ -1,10 +1,18 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from game_state import State
+
+if TYPE_CHECKING:
+    from pykraken import Event
+
 
 __all__ = ("BaseState",)
 
 
 class BaseState(State["BaseState"]):
-    def process_update(self, dt: float | None) -> None:  # pyright: ignore[reportIncompatibleMethodOverride]
+    def process_update(self, dt: float) -> None:  # pyright: ignore[reportIncompatibleMethodOverride]
         ...
+
+    def process_event(self, event: Event) -> None: ...
