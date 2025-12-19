f40d61b9ce27b7ea8e6a1698cb82032b669cdc01
Jiggly Balls <jigglyballs9000@gmail.com>
2025-12-19 19:44:07 +0530

    Disabled auto loading

--- states/game.py
+++ states/game.py
@@ -1,8 +1,4 @@
 from states.meta import BaseState, StateEnum
 
 
-class GameState(
-    BaseState,
-    state_name=StateEnum.GAME,
-    eager_load=True,
-): ...
+class GameState(BaseState, state_name=StateEnum.GAME): ...
