cf609975a36ff65e23234bd570abfa1f5f3af376
Jiggly Balls <jigglyballs9000@gmail.com>
2025-12-20 18:38:09 +0530

    Finished player movement & animation

--- entities/player.py
+++ entities/player.py
@@ -1,13 +1,63 @@
 from __future__ import annotations
 
+from enum import StrEnum, auto
 from typing import TYPE_CHECKING
 
-from pykraken import Sprite
+import pykraken as kn
+from pykraken import Vec2
 
 if TYPE_CHECKING:
-    from pykraken import AnimationController
+    from pykraken import AnimationController, Event, Scancode
 
 
-class Player(Sprite):
-    def __init__(self, animation: AnimationController) -> None:
+class PlayerStates(StrEnum):
+    IDLE = auto()
+
+
+class Player:
+    UP: tuple[Scancode, Scancode] = (kn.S_w, kn.S_UP)
+    DOWN: tuple[Scancode, Scancode] = (kn.S_s, kn.S_DOWN)
+    RIGHT: tuple[Scancode, Scancode] = (kn.S_d, kn.S_RIGHT)
+    LEFT: tuple[Scancode, Scancode] = (kn.S_a, kn.S_LEFT)
+
+    def __init__(
+        self,
+        animation: AnimationController,
+        current_state: PlayerStates,
+        position: None | Vec2 = None,
+    ) -> None:
         super().__init__()
+
+        self.animation: AnimationController = animation
+        self.current_state: PlayerStates = current_state
+        self.position: Vec2 = position or Vec2()
+
+        self.speed: int = 200
+        self.direction: Vec2 = Vec2()
+
+    def process_event(self, event: Event) -> None:
+        if event.type == kn.KEY_DOWN:
+            if event.scan in self.UP:
+                self.direction.y = -1
+            elif event.scan in self.DOWN:
+                self.direction.y = 1
+
+            if event.scan in self.LEFT:
+                self.direction.x = -1
+            elif event.scan in self.RIGHT:
+                self.direction.x = 1
+
+        elif event.type == kn.KEY_UP:
+            if event.scan in self.UP or event.scan in self.DOWN:
+                self.direction.y = 0
+
+            if event.scan in self.LEFT or event.scan in self.RIGHT:
+                self.direction.x = 0
+
+        if self.direction.length != 0:
+            self.direction.normalize()
+
+    def process_update(self, dt: float) -> None:
+        x_magnitude = self.direction.x * self.speed * dt
+        y_magnitude = self.direction.y * self.speed * dt
+        self.position += kn.Vec2(x_magnitude, y_magnitude)
--- states/meta/loader.py
+++ states/meta/loader.py
@@ -1,11 +1,26 @@
+from pykraken import AnimationController, SheetStrip, Vec2
+
+from core.constants import ASSET_SOLDIER
+from entities.player import Player, PlayerStates
 from states.meta.base_state import BaseState
 from states.meta.state_enums import StateEnum
 
 
 class LoaderState(BaseState, state_name=StateEnum.LOADER):
-    def on_load(self, reload: bool) -> None:
-        if reload:
-            return
+    def _load_player(self) -> None:
+        sprite_sheet: list[SheetStrip] = [SheetStrip(PlayerStates.IDLE, 6, 2)]
 
-        # Add asset loading etc...
+        player_animation = AnimationController()
+        player_animation.load_sprite_sheet(
+            ASSET_SOLDIER, Vec2(100, 100), sprite_sheet
+        )
+
+        BaseState.player = Player(player_animation, PlayerStates.IDLE)
+
+    def _load_keybinds(self) -> None: ...
+
+    def on_enter(self, previous_state: None | BaseState) -> None:
+        self._load_player()
+        self._load_keybinds()
+
         self.manager.change_state(self.manager.post_init_state)
