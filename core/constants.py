4483eebbc20dc5b5da17ed2ab110c736e13b90df
Jiggly Balls <jigglyballs9000@gmail.com>
2025-12-22 00:43:32 +0530

    Renamed constants

--- core/constants.py
+++ core/constants.py
@@ -7,5 +7,5 @@ WINDOW_HEIGHT: int = 500
 PLAYER_ANIMATION_FPS: int = 7
 
 ASSET_HUMAN_IDLE: str = "assets/graphics/human/IDLE/base_idle_strip.png"
-ASSET_HUMAN_WALK: str = "assets/graphics/human/WALKING/base_walk_strip.png"
-ASSET_HUMAN_RUN: str = "assets/graphics/human/RUN/base_run_strip.png"
+ASSET_HUMAN_WALKING: str = "assets/graphics/human/WALKING/base_walk_strip.png"
+ASSET_HUMAN_RUNNING: str = "assets/graphics/human/RUN/base_run_strip.png"
