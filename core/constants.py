7bfb0e7ab60191011c1ba957f41f48fd36d887a9
Jiggly Balls <jigglyballs9000@gmail.com>
2025-12-22 13:55:44 +0530

    Updated target res

--- core/constants.py
+++ core/constants.py
@@ -5,6 +5,7 @@ WINDOW_WIDTH: int = 700
 WINDOW_HEIGHT: int = 500
 
 PLAYER_ANIMATION_FPS: int = 15
+PLAYER_TARGET_RES: tuple[int, int] = (288, 192)
 
 ASSET_HUMAN_IDLE: str = "assets/graphics/human/IDLE/base_idle_strip.png"
 ASSET_HUMAN_WALKING: str = "assets/graphics/human/WALKING/base_walk_strip.png"
--- states/game.py
+++ states/game.py
@@ -2,14 +2,11 @@ from __future__ import annotations
 
 import pykraken as kn
 
-from entities.player import PlayerStates
+from core.constants import PLAYER_TARGET_RES
 from states.meta import BaseState, StateEnum
 
 
 class GameState(BaseState, state_name=StateEnum.GAME):
-    def on_enter(self, previous_state: None | BaseState) -> None:
-        self.player.animator.change_animation(PlayerStates.RUNNING)
-
     def process_update(self, dt: float) -> None:
         self.player.process_update(dt)
 
@@ -19,6 +16,6 @@ class GameState(BaseState, state_name=StateEnum.GAME):
 
         kn.renderer.draw(
             player_texture,
-            kn.Transform(self.player.position),
+            kn.Transform(self.player.position, size=PLAYER_TARGET_RES),
             src=player_clip,
         )
